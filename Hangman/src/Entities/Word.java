562125758c9e80a230c75371f203f9b145fc743b
NINJAMUSICIAN <j80mar4@gmail.com>
2013-12-18 02:31:27 -0700

    Added Some Super Fun Stuff!!!

--- Hangman/src/Entities/Word.java
+++ Hangman/src/Entities/Word.java
@@ -1,37 +1,60 @@
 package Entities;
 
+import Main.PlayState;
+
 public class Word {
 
-	private String word = "drag";
+	String[] words;
+	
+	private String word = "rdag";
 	char[] cArray = word.toCharArray();
 	private String[] rightGuessed = new String[10];
 	private String[] wrongGuessed;
 	
 	private int amountRight;
 	
-	public Word(){
-		
+	private PlayState playstate;
+	
+	public Word(PlayState p){
+		playstate = p;
 	}
 	
 	public String getWord(){return word;}
 	public void setWord(String s){word = s;}
 	
+	public int findWhere(String s){
+		int here = -1;
+		for(int i = 0; i < words.length; i++){
+				if(s.equalsIgnoreCase(words[i])){
+				here = i;
+			}
+		}
+		System.out.println(here);
+		return here;
+	}
+	
 	public void checkGuess(String s){
+		int temp = 0;
+		
 		if(word.contains(s)){
 			System.out.println("right!");
-			rightGuessed[1] = s;
-			amountRight++;
-			for(int i = 0; i < amountRight; i++){
-			}
-			System.out.println("correct guess was " + rightGuessed[1]);
+			System.out.println("correct guess was " + s);
 			
+			temp = findWhere(s);
+			playstate.setGuessed(playstate.getGuessesMade(), s);
 		}else{
 			System.out.println("wrong");
+			System.out.println("you guessed " + s);
+			
+			
+			temp = findWhere(s);
+			playstate.setGuessed(playstate.getGuessesMade(), s);
+			
 		}
 	}
 	
 	public void splitWord(){
-		String[] words = word.split("(?!^)");
+		words = word.split("(?!^)");
 		for(int i = 0; i < words.length; i++){
 			System.out.println(words[i]);
 		}
--- Hangman/src/Main/PlayState.java
+++ Hangman/src/Main/PlayState.java
@@ -11,15 +11,44 @@ public class PlayState {
 	private Word word;
 	private long waitTimer;
 	private int started = 0;
-	private boolean able = true, guessed = false;;
+	private int guessesMade = 0;
+	private boolean able = true, guessed = false;
+	
+	private String[] guessedLets;
+	private String[] wrongLets;
+	private String[] rightLets;
 	
 	public PlayState(){
 		init();
 	}
 	
 	public void init(){
-		word = new Word();
+		word = new Word(this);
 		word.splitWord();
+		
+		guessedLets = new String[word.getWord().length() + 6];
+		wrongLets = new String[6];
+		rightLets = new String[word.getWord().length()];
+	}
+	
+	public void setGuessed(int num, String let){//which place in array && what the letter is to be put in
+		guessedLets[num] = let;
+		for(int i = 0; i < guessedLets.length; i++){
+			System.out.println("letter guessed so far " + guessedLets[i]);
+		}
+	}
+	public void setRight(int num, String let){//which place in array && what the letter is to be put in
+		rightLets[num] = let;
+			for(int i = 0; i < rightLets.length; i++){
+				System.out.println("right letters so far " + rightLets[i]);
+			}
+		}
+	
+	public void setWrong(int num, String let){
+		wrongLets[num] = let;
+		for(int i = 0; i < wrongLets.length; i++){
+			System.out.println("right letters so far " + wrongLets[i]);
+		}
 	}
 	
 	public void setAble(boolean b){
@@ -55,16 +84,28 @@ public class PlayState {
 	g.fillRect(0, 0, 640, 480);
 	}
 
+	public void checkIt(String g){
+		word.checkGuess(g);
+		able = false;
+		guessed = true;
+		guessesMade++;
+		System.out.println(guessesMade);
+		startTimer();
+		started = 1;
+		moveWait();
+	}
+	
 	public void handleInput(){
 		if(able){
+			if(Keys.isPressed(Keys.C)){
+				checkIt("c");
+				}
 		if(Keys.isPressed(Keys.D)){
-			word.checkGuess("d");
-			able = false;
-			guessed = true;
-			startTimer();
-			started = 1;
-			moveWait();
+			checkIt("d");
 			}
 		}
 	}
+
+	public int getGuessesMade(){return guessesMade;}
+
 }
