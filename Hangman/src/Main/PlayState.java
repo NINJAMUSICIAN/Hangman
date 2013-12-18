a271d8c92fce17220247a82a1d36845eda95b4f7
NINJAMUSICIAN <j80mar4@gmail.com>
2013-12-18 01:16:32 -0700

    Does this work now?

--- Hangman/src/Main/PlayState.java
+++ Hangman/src/Main/PlayState.java
@@ -10,6 +10,7 @@ public class PlayState {
 
 	private Word word;
 	private long waitTimer;
+	private boolean able = true;
 	
 	public PlayState(){
 		init();
@@ -19,12 +20,19 @@ public class PlayState {
 		word = new Word();
 		word.splitWord();
 	}
-
+	
+	public void setAble(boolean b){
+		able = b;
+	}
+	
 	public void moveWait(){ //counts down time before you can guess again
+		setAble(false);
 		waitTimer = System.nanoTime();
 		long elapsed = (System.nanoTime() - waitTimer) / 1000000;
+		
 		if(elapsed > 1000){
 			System.out.println("goNow");
+			setAble(true);
 		}
 	}
 	
@@ -38,9 +46,11 @@ public class PlayState {
 	}
 
 	public void handleInput(){
-		
+		if(able){
 		if(Keys.isPressed(Keys.D)){
 			word.checkGuess("d");
+			moveWait();
+			}
 		}
 	}
 }
